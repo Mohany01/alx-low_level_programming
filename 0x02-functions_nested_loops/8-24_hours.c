32f4d5265bced4415878da5a77d8b31552415558
Mohany01 <madagames01@gmail.com>
2023-08-15 11:11:37 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,9 @@
 void jack_bauer(void)
 {
 int hr, min;
-for (int hr = 0; hr <= 23; hr++)
+for ( hr = 0; hr <= 23; hr++)
 {
-for (int min = 0; min <= 59; min++)
+for ( min = 0; min <= 59; min++)
 {
 _putchar (hr / 10 + '0');
 _putchar (hr % 10 + '0');
