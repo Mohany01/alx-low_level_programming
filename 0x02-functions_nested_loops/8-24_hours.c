b8a4e92e019359a487123eea03c436280815907d
Mohany01 <madagames01@gmail.com>
2023-08-15 11:08:56 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,10 +10,11 @@ for (int hr = 0; hr <= 23; hr++)
 {
 for (int min = 0; min <= 59; min++)
 {
-putchar (hr / 10 + '0');
-putchar (hr % 10 + '0');
-putchar (':');
-putchar (min / 10 + '0');
-putchar (min % 10 + '0');
+_putchar (hr / 10 + '0');
+_putchar (hr % 10 + '0');
+_putchar (':');
+_putchar (min / 10 + '0');
+_putchar (min % 10 + '0');
+}
 }
 }
